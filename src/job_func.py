85a239533a8b9f95a3eb586bedadf20c67b715ab
xsqian <xsqian@gmail.com>
2023-10-27 15:50:05 +0000

    resolve the error of No module named 'mylib'

--- src/job_func.py
+++ src/job_func.py
@@ -15,7 +15,7 @@
 # test loading repo from git in different mlrun runtimes
 # contain function code (this) and locally imported lib (mylib)
 
-from mylib import myfunc
+from src.mylib import myfunc
 
 # mlrun job runtime entry
 def job_handler(context):
